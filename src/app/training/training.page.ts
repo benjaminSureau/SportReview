d7f406a23e494d51bfe902cbced116389806709d
Enzo Scaduto <enzo.scaduto@gmail.com>
2019-06-06 12:40:25 +0200

    Add alert training

--- src/app/training/training.page.ts
+++ src/app/training/training.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-training',
@@ -10,14 +11,14 @@ export class TrainingPage implements OnInit {
 
   private allTraining = ['Calories', 'Endurance', 'Bike'];
 
-  constructor(public actionSheetController: ActionSheetController) { }
+  constructor(public actionSheetController: ActionSheetController, public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  async presentActionSheet() {
+  async presentActionSheet(text: string) {
     const actionSheet = await this.actionSheetController.create({
-      header: 'Albums',
+      header: text,
       buttons: [{
         text: 'Delete',
         role: 'destructive',
@@ -29,6 +30,7 @@ export class TrainingPage implements OnInit {
         text: 'Edit',
         icon: 'create',
         handler: () => {
+          this.presentAlertEditTraining(text);
           console.log('Share clicked');
         }
       }, {
@@ -41,5 +43,140 @@ export class TrainingPage implements OnInit {
       }]
     });
     await actionSheet.present();
+  }
+
+  onClickNew() {
+    this.presentAlertNewTraining();
+  }
+
+  async presentAlertNewTraining() {
+    const alert = await this.alertController.create({
+      header: 'New',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Title'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: 0,
+          max: 10000
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertEditTraining(name: string) {
+    const alert = await this.alertController.create({
+      header: 'Edit',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          value: name,
+          placeholder: 'Title'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: 0,
+          max: 10000
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
