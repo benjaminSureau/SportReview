991b0e706fbe1ae79312bc08021f5ab1e408ce09
Enzo Scaduto <enzo.scaduto@gmail.com>
2019-06-06 11:58:00 +0200

    Add button to training Page, click on button display menu to edit or remove training, add button +

--- src/app/training/training.page.ts
+++ src/app/training/training.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-training',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingPage implements OnInit {
 
-  constructor() { }
+  private allTraining = ['Calories', 'Endurance', 'Bike'];
+
+  constructor(public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
 
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Edit',
+        icon: 'create',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
